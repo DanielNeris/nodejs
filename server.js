b7f7c76dc0ebd1b689455fec1d2ee44956130810
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-06-24 08:43:19 -0300

    Criando model de produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
@@ -7,9 +8,18 @@ const app = express();
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodoapi', { useNewUrlParser: true });
 
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 //Primeira rota
 app.get('/', (req, res) => {
-    res.send("Hello guys");
+    Product.create({ 
+        title: 'ReactJS', 
+        description: 'Learning ReactJS/Native and NodeJs',
+        url: 'http://github.com/facebook/react'
+    });
+    return res.send("Hello guys");
 });
 
 app.listen(3001);
