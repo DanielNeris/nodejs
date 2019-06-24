3eb9bd10be24eb88ee7c69504c4a475ca7ec28ef
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-06-24 10:13:30 -0300

    Adicionando CORS

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodoapi', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
 requireDir('./src/models');
 
