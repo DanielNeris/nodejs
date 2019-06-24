8a15fb2393cd4fba49b02a80b5f1ca7fdae63b72
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-06-24 08:14:22 -0300

    Utilizando Nodemon

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello");
-    console.log(req, res);
+    res.send("Hello guys");
 });
 
 app.listen(3001);
